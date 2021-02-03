0c2444e5e09e8c1bf22a08d7d6afc1c8c74156c8
Nikita <nikitadedov66@gmail.com>
2021-02-03 20:25:06 +0200

    enemyspikes realized / need fix when level mirrored

--- Assets/Scripts/EnemySpikes.cs
+++ Assets/Scripts/EnemySpikes.cs
@@ -3,66 +3,98 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpikes : Enemy {
+
     [SerializeField]
     float timer;
 
     bool inZone;
 
     [SerializeField]
-    Animator animator;
-
-  // [SerializeField]
-    AnimationCurve curve;
-
-    [SerializeField]
     GameObject enemyPrefab;
 
-    float xBefore;
-    float xAfter;
+    Vector3 startPos;
+    Vector3 nextPos;
+
+    [SerializeField]
+    float sneakSpeed;
+
+    [SerializeField]
+    float distanceToSneak;
+
+    [SerializeField]
+    float distanceToNextPos;
+
+    bool allowToAttack = true;
+
+    bool attackIsOver = true;
+
+    [SerializeField]
+    GameObject deathCollider;
 
     public void SetColor() {
 
     }
 
     public override void Attack() {
-        animator.SetTrigger("Attack");
+        StartCoroutine(AttackCoroutine());
+    }
+
+    IEnumerator AttackCoroutine() {
+        allowToAttack = false;
+        attackIsOver = false;
+
+        bool over = false;
+        Debug.Log("Attack");
+        var nextPoz = enemyPrefab.transform.position + new Vector3(transform.localScale.x * -0.45f, 0, 0);
+
+        var temp = enemyPrefab.transform.localScale;
+        deathCollider.SetActive(true);
+
+        while (!over) {
+
+            temp = temp * 1.3f;
+            enemyPrefab.transform.localScale = temp;
+            enemyPrefab.transform.position = Vector3.Lerp(enemyPrefab.transform.position, nextPoz, 0.5f);
+
+            if (Vector3.Distance(enemyPrefab.transform.position, nextPoz) < 0.2f && temp.x > 0.5f) {
+                over = true;
+                attackIsOver = true;
+            }
+
+            yield return new WaitForSeconds(0.2f);
+        }
+
+
+        allowToAttack = true;
+
+        yield return new WaitForSeconds(1f);
+        deathCollider.SetActive(false);
     }
 
     private void Start() {
-        xBefore = enemyPrefab.transform.localPosition.x;
-        xAfter = xBefore + 1;
+        startPos = enemyPrefab.transform.position;
+        nextPos = startPos + new Vector3(transform.localScale.x * distanceToSneak, 0, 0);
+    }
 
+    private void OnDrawGizmos() {
+        Gizmos.DrawWireSphere(nextPos, 0.1f);
+        Gizmos.DrawWireSphere(enemyPrefab.transform.position, 0.3f);
     }
 
     private void Update() {
+        distanceToNextPos = Vector3.Distance(enemyPrefab.transform.position, nextPos);
+
         if (!inZone) {
             timer -= Time.deltaTime / Time.timeScale;
             if (timer < 0) timer = 0;
-        }
 
-        if (animator.GetBool("Sneak")) {
-            animator.enabled = false;
-
-            if (Input.GetKeyDown(KeyCode.G)) {
-            //    curve.RemoveKey(1);
-            //    curve.RemoveKey(0);
-
+            if (attackIsOver) {
+                SneakOut();
             }
-
-          //  curve.AddKey(0, enemyPrefab.transform.position.x);
-          //  curve.AddKey(1, enemyPrefab.transform.position.x + 2);
-
-            curve = new AnimationCurve(new Keyframe { value = xBefore, time = 0 },
-                new Keyframe { value = xAfter , time = 1 });
-
-           
-            
-            Debug.Log(curve.keys[0].value);
-            enemyPrefab.transform.position = new Vector3(-curve.Evaluate(timer), enemyPrefab.transform.position.y, enemyPrefab.transform.position.z);
         }
 
-        if (!animator.GetBool("Sneak")) {
-            animator.enabled = true;
+        if (timer < 0.49 && inZone && allowToAttack) {
+            SneakIn();
         }
 
     }
@@ -70,7 +102,6 @@ public class EnemySpikes : Enemy {
 
     private void OnTriggerEnter(Collider other) {
         inZone = true;
-        animator.SetBool("Sneak", true);
     }
 
     private void OnTriggerStay(Collider other) {
@@ -78,18 +109,34 @@ public class EnemySpikes : Enemy {
         if (timer > 0.9999) {
             timer = 1;
         }
-
-
-
-        if (timer > 0.5f) {
+        if (timer > 0.7f && distanceToNextPos < 0.25f && allowToAttack) {
             Attack();
         }
+
+
     }
 
     private void OnTriggerExit(Collider other) {
         inZone = false;
-        animator.SetBool("Sneak", false);
     }
+
+    void SneakIn() {
+        nextPos = new Vector3(nextPos.x , nextPos.y, nextPos.z);
+        enemyPrefab.transform.position = Vector3.Lerp(enemyPrefab.transform.position, nextPos, sneakSpeed);
+    }
+    void SneakOut() {
+
+        var temp = enemyPrefab.transform.localScale;
+        if (enemyPrefab.transform.localScale.x > 0.51f) {
+            temp *= 0.99f;
+            enemyPrefab.transform.localScale = temp;
+        }
+
+
+
+        enemyPrefab.transform.position = Vector3.Lerp(enemyPrefab.transform.position, startPos, sneakSpeed);
+    }
+
 
 
 
---
+++ Assets/Scripts/EnemySpikes_SnekaBehaviour.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpikes_SnekaBehaviour : StateMachineBehaviour
+{
+
+    Vector3 startPos;
+    Vector3 nextPos;
+
+    float timer;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        startPos = Vector3.zero;
+        nextPos = startPos + new Vector3(2, 0, 0);
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+        while (true) {
+            timer += Time.deltaTime / Time.timeScale;
+
+            if(timer > 0.1) {
+                timer = 0;
+                return;
+            }
+
+            var distance = Vector3.Distance(nextPos,animator.transform.position);
+
+            if(distance > 0.01) {
+                animator.transform.position += new Vector3(distance, 0, 0);
+            }
+            else {
+                animator.transform.position -= new Vector3(distance, 0, 0);
+            }
+
+        }
+
+    }
+  
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+   
+}
