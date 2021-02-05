a92b46cc0ffbf0e2edd4296601081f81ca50bde8
Dan <danilka2315.g@gmail.com>
2021-02-05 23:41:13 +0600

    Летающий моб
    буль буль буль

---
+++ Assets/Scripts/FlyingEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemy : MonoBehaviour
+{
+    [SerializeField]
+    Transform playerPosition;
+
+    [SerializeField]
+    float provocationRadius;
+
+    [SerializeField]
+    float accelerationFactor;
+
+    Vector3 velocity = Vector3.zero;
+
+    private void Start()
+    {
+        playerPosition = PlayerControl.Instance.transform;
+    }
+
+    void Update()
+    {
+        Vector3 distance = transform.position - playerPosition.position;
+        if ((distance).magnitude < provocationRadius)
+        {
+            Vector3 acceleration = distance.normalized * accelerationFactor;
+            velocity = velocity * 0.9f + acceleration * Time.deltaTime;
+            transform.Translate(-velocity);
+        }
+    }
+}
